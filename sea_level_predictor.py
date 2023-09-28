a886024a1652fe72a894eb54a2dc57cd691d766a
Replit user <>
2023-09-28 17:27:22 +0200

    FCC Sea Level Pred._cert. Project

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,37 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+  """Creates a scatter plot of sea level data with two lines of best fit.
 
+  Returns:
+    The plot object.
+  """
 
-    # Create scatter plot
+  # Read data from file
+  data = pd.read_csv('epa-sea-level.csv')
 
+  # Create scatter plot
+  plt.scatter(data['Year'], data['CSIRO Adjusted Sea Level'], edgecolors='g')
 
-    # Create first line of best fit
+  # Create line of best fit for entire dataset
+  slope, intercept, r_value, p_value, std_err = linregress(data['Year'], data['CSIRO Adjusted Sea Level'])
+  plt.plot(data['Year'], intercept + slope * data['Year'], 'k')
 
+  # Create line of best fit for data from year 2000 onwards
+  slope, intercept, r_value, p_value, std_err = linregress(data['Year'][data['Year'] >= 2000], data['CSIRO Adjusted Sea Level'][data['Year'] >= 2000])
+  plt.plot(data['Year'][data['Year'] >= 2000], intercept + slope * data['Year'][data['Year'] >= 2000], 'b')
 
-    # Create second line of best fit
+  # Add labels and title
+  plt.xlabel('Year')
+  plt.ylabel('Sea Level (inches)')
+  plt.title('Rise in Sea Level')
 
+  # Draw lines of best fit through year 2050
+  plt.plot([1880, 2050], [intercept, intercept + slope * 2050], 'k')
+  plt.plot([2000, 2050], [intercept, intercept + slope * 2050], 'b')
 
-    # Add labels and title
+  return plt.gca()
 
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
-    return plt.gca()
+# Draw plot and save it as a PNG image
+plot = draw_plot()
+plt.savefig('sea_level_plot.png')
